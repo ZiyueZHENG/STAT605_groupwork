09476c4be74329ccd275d1daea801d547083dead
ZiyueZHENG <75109830+ZiyueZHENG@users.noreply.github.com>
2021-12-08 17:26:35 -0600

    Update statistical_test.R

--- analysis/statistical_test.R
+++ analysis/statistical_test.R
@@ -1,3 +1,8 @@
+library(cars)
+library(quantmod)
+library(userfriendlyscience)
+library(ggplot2)
+
 # Get data
 data1 = read.csv('all_processed.csv',header = F)
 colnames(data1) = c('Date','Week','Volume','Turnover','AvgPrice','Opening Price','Closing Price','MaxPrice','MinPrice','NetInflow')
@@ -8,16 +13,18 @@ for(i in 1:633){
 }
 
 # Data visualization: Candlestick chart
-library(quantmod)
 getSymbols('BTCUSD',src = "csv")
 chartSeries(BTCUSD)
 
 # Box plot
-library(ggplot2)
 ggplot(data1,aes(x=Week,y=return))+
   geom_boxplot()
 ggplot(data1,aes(x=Week,y=Volume))+
   geom_boxplot()
+
+# Homogeneity of variance test
+leveneTest(return~Week,data = data1) # return is heteroscedasticity
+leveneTest(Volume~Week,data = data1)
 
 # two sample t-test
 tt = c()
@@ -30,11 +37,10 @@ for(i in 1:6){
 }
 print(tt)# Return between 6&7,3&7 is significant under 95% level.
 
-# Tuckey test
-summary(fm1 <- aov(return ~ Week, data = data1))
-TukeyHSD(fm1, "Week", ordered = F)
-plot(TukeyHSD(fm1, "Week"))
+# Using Games-Howell test with return (because of heteroscedasticity)
+oneway.test(return~Week,data = data1)
 
+# Using Tuckey test with trading volume
 summary(fm2 <- aov(Volume ~ Week, data = data1))
 TukeyHSD(fm2, "Week", ordered = F)
 plot(TukeyHSD(fm2, "Week"))
